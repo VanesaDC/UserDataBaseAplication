11d9d9167b366c2f9e235469d0927fc30320a038
Vanesa Díaz <studiosvanesa@gmail.com>
2023-06-19 23:12:48 +0100

    a warning about module-info.java was solved

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.mysqlfx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.example.mysqlfx to javafx.fxml;
