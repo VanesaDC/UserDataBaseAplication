96c3ed779457b4115cd292d970c2c1408a99e993
Vanesa Díaz <studiosvanesa@gmail.com>
2023-06-20 10:49:14 +0100

    🙂 secondWindow.fxml created

--- src/main/java/com/UI/HelloController.java
+++ src/main/java/com/UI/HelloController.java
@@ -1,7 +1,14 @@
 package com.UI;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -9,6 +16,20 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("secondWindow.fxml"));
+        try {
+            Parent root = loader.load();
+            SecondWindowController controller = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         welcomeText.setText("Welcome to JavaFX Application!");
     }
