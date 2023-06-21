b68e62735c41a95391f0da3de8191b5a6ad3f264
Vanesa Díaz <studiosvanesa@gmail.com>
2023-06-21 11:02:36 +0100

    🟢Age should_throw_a_exception_when_it_contains_letters()

--- src/main/java/Domain/Age.java
+++ src/main/java/Domain/Age.java
@@ -1,5 +1,7 @@
 package Domain;
 
+import java.util.regex.Pattern;
+
 public class Age {
     private String age;
 
@@ -8,16 +10,20 @@ public class Age {
     }
 
     public static Age createAge(String number) throws AgeException{
+        Pattern patron = Pattern.compile("[0-9]+");
+        boolean notIsNumeric = !(patron.matcher(number).find());
+        if (notIsNumeric) {
+            throw new AgeException("Escriba la edad con números, por favor.");
+        }
         int age = Integer.parseInt(number);
         boolean ageIsLessThanZero = age<0;
         boolean ageIsGreaterThanAHundred = age >100;
-
         if (ageIsLessThanZero) {
             throw new AgeException("La edad no puede ser menor que cero.");
         }
-        if (ageIsGreaterThanAHundred)
+        if (ageIsGreaterThanAHundred) {
             throw new AgeException("La edad no puede superar los 100 años.");
-
+        }
         return new Age(number);
     }
 }
